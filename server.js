3ecc7150e609b0c958842c6e09cf942653501af5
Jay Williams <course@jaywilliams.me>
2017-12-28 16:07:56 +0000

    Better auth, also beautify spec file

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var app = express();
-var server = require('http').Server(app);
+var server = require('http')
+  .Server(app);
 var io = require('socket.io')(server);
 var bodyParser = require('body-parser');
 var hashPassword = require("./helpers/hashPassword");
@@ -9,9 +10,10 @@ var si = require("systeminformation");
 var jwt = require("jsonwebtoken");
 var fs = require("fs");
 var csp = require("chmod-style-permissions");
-
+var ejs = require("ejs");
 var sessionMiddleware = session({
-  secret: Math.floor(Math.random() * 100000000000).toString(16)
+  secret: Math.floor(Math.random() * 100000000000)
+    .toString(16)
 });
 io.use(function (socket, next) {
   sessionMiddleware(socket.request, socket.request.res, next);
@@ -20,40 +22,38 @@ app.use(sessionMiddleware);
 app.use(bodyParser.urlencoded({
   extended: true
 }));
+
+function requirePermission() {
+  var _arguments = arguments;
+  /**
+   * @type {function}
+   */
+  return function (req, res, next) {
+    _arguments.every(node => {
+      return csp.hasPerm(request.session.user.permissions, node)
+    }) ? next() : res.redirect("/")
+  }
+}
 app.use(bodyParser.json());
 app.use(bodyParser.raw());
-app.use(express.static("assets"))
+app.use(express.static("assets"));
+app.set("view engine", ejs);
 server.listen(8008);
-
 app.get('/', function (req, res) {
   res.redirect("http://jwte.ch:5678/requestauth?redirect=" + encodeURIComponent("http://jwte.ch:8008/callback?token=$token"))
 });
-app.get('/panel', function (req, res) {
-  if (req.session.authed) {
-    res.sendfile(__dirname + '/html/panel.html');
-  } else {
-    res.end("Unauthorized")
-  }
-
+app.get('/panel', requirePermission("nightborn.panel.access"), function (req, res) {
+  res.sendfile(__dirname + '/html/panel.html');
 });
-app.get('/botlogs', function (req, res) {
-  if (req.session.authed) {
-    res.sendfile(__dirname + '/html/botlogs.html');
-  } else {
-    res.end("Unauthorized")
-  }
+app.get('/botlogs', requirePermission("nightborn.panel.access"), function (req, res) {
+  res.sendfile(__dirname + '/html/botlogs.html');
 });
-app.get('/m_manager', function (req, res) {
-  if (req.session.authed) {
-    res.sendfile(__dirname + '/html/memberOptions.html');
-  } else {
-    res.end("Unauthorized")
-  }
+app.get('/m_manager', requirePermission("nightborn.panel.access"), function (req, res) {
+  res.sendfile(__dirname + '/html/memberOptions.html');
 });
-
 app.get("/callback", (req, res) => {
   var token = req.query.token;
-  if(!token) return res.end("go away");
+  if (!token) return res.end("go away");
   try {
     var tokendata = jwt.verify(token, fs.readFileSync("./public.key"))
     console.log("passed 1st check")
@@ -62,10 +62,11 @@ app.get("/callback", (req, res) => {
     return res.end("go away")
   }
   if ((tokendata.logintime + 2000) < Date.now()) return res.end("You're not authorized to do this.")
-  if (!csp.hasPerm(tokendata.permissions, "nightborn.panel")) return res.end("You're not authorized to do this.")
+  if (!csp.hasPerm(tokendata.permissions, "nightborn.panel.login")) return res.end("You're not authorized to do this.")
   console.log("passed 2nd check")
   req.session.authed = true
   req.session.username = tokendata.username;
+  req.session.user = tokendata;
   res.redirect("/panel")
 })
 app.post("/auth", (req, res) => {
@@ -85,37 +86,38 @@ io.on('connection', function (socket) {
   if (socket.request.session.authed) {
     console.log("Authed request")
     socket.on("request.sysinfo", () => {
-      Promise.all([si.mem(), si.currentLoad()]).then((results) => {
-        var m = results[0];
-        var cl = results[1];
-        socket.emit("response.sysinfo", {
-          ram: {
-            percent: Math.floor((m.used / m.total) * 100).toString() + "%",
-            used: m.used,
-            free: m.free,
-            total: m.total,
-            human: Math.floor((m.used / 1000) / 1000) + "mb"
-          },
-          cpu: {
-            percent: cl.currentload + "%",
-            load: cl.currentload
-          }
-        });
-      })
+      Promise.all([si.mem(), si.currentLoad()])
+        .then((results) => {
+          var m = results[0];
+          var cl = results[1];
+          socket.emit("response.sysinfo", {
+            ram: {
+              percent: Math.floor((m.used / m.total) * 100)
+                .toString() + "%",
+              used: m.used,
+              free: m.free,
+              total: m.total,
+              human: Math.floor((m.used / 1000) / 1000) + "mb"
+            },
+            cpu: {
+              percent: cl.currentload + "%",
+              load: cl.currentload
+            }
+          });
+        })
     })
     socket.on("forward", (req) => {
       io.emit(req.event, req.packet);
     })
   }
-
   socket.on("member.find", function (tf) {
     tf.replyId = socket.id;
     io.emit("bot.find", tf);
   })
   socket.on("bot.found", function (res) {
-    io.to(res.replyId).emit("member.found", res)
+    io.to(res.replyId)
+      .emit("member.found", res)
   })
-
   socket.on("sendLogsSoon <3", function () {
     console.log("Bot connected!");
   });
